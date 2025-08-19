926cde784bb92f1d3a0562c6d5337cf222e640c8
Pradeep Chandran M <pradeepchandranm@gmail.com>
2025-08-19 15:22:07 +0530

    backend added

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Navbar from "./components/NavBar";
+import ViewPlayer from "./components/ViewPlayer";
+import Home from "./components/Home";
+import Footer from "./components/Footer";
+import { Route, BrowserRouter, Routes } from "react-router-dom";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Navbar />
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/players" element={<ViewPlayer />} />
+        </Routes>
+      </div>
+      <Footer />
+    </BrowserRouter>
   );
 }
-
 export default App;
