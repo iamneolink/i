4d1a9192e198411c2b9c9e6dc4b2116c50060312
Pradeep Chandran M <pradeepchandranm@gmail.com>
2025-08-19 12:19:04 +0530

    local change

--- reactapp/src/components/NavBar.jsx
+++ reactapp/src/components/NavBar.jsx
@@ -1,11 +1,9 @@
 import React from "react";
 
-function NavBar()
-{
-    <div>
-        <h1>Neo Cricket Tournament Registration</h1>
-        
-    </div>
+function NavBar() {
+  <div>
+    <h1>Neo Cricket changed locally Registration</h1>
+  </div>;
 }
 
 export default NavBar;
