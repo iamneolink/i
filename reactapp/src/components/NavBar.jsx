d04755933bd2f8dabe5441147a5d44537223fc31
RAN-GAN <pradeepmojo1708@gmail.com>
2025-08-19 03:50:30 +0000

    initial commit

---
+++ reactapp/src/components/NavBar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function NavBar()
+{
+    <div>
+        <h1>Neo Cricket Tournament Registration</h1>
+        
+    </div>
+}
+
+export default NavBar;
---
+++ springapp/src/test/java/com/examly/springapp/SpringappApplicationTests.java
@@ -0,0 +1,12 @@
+package com.examly.springapp;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
