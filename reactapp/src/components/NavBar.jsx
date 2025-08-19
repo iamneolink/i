79a84db0b3169aecb5a23831ec137ccb49c52ccd
Pradeep Chandran M <pradeepchandranm@gmail.com>
2025-08-19 12:51:20 +0530

    kfjsdksbf

--- reactapp/src/components/NavBar.jsx
+++ reactapp/src/components/NavBar.jsx
@@ -2,8 +2,7 @@ import React from "react";
 
 function NavBar() {
   <div>
-    <h1>Neo Cricket changed locally AGAIN Registration</h1>
+    <h1>Neo Cricket changed locally AGAIN again Registration</h1>
   </div>;
 }
-
 export default NavBar;
