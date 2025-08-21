484e14ed7f0aea05940a65cc984e912f10aadbce
Pradeep Chandran M <pradeepchandranm@gmail.com>
2025-08-21 15:13:59 +0530

    fixed bg

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -5,19 +5,21 @@ import Home from "./components/Home";
 import ViewPlayer from "./components/ViewPlayer";
 import AddPlayer from "./components/AddPlayer";
 import Footer from "./components/Footer";
-
+import "./App.css";
 function App() {
   return (
     <BrowserRouter>
-      <Navbar />
-      <div className="container">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/players" element={<ViewPlayer />} />
-          <Route path="/add" element={<AddPlayer />} />
-        </Routes>
+      <div className="app-flex-wrapper">
+        <Navbar />
+        <div className="container" style={{ flex: 1 }}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/players" element={<ViewPlayer />} />
+            <Route path="/add" element={<AddPlayer />} />
+          </Routes>
+        </div>
+        <Footer />
       </div>
-      <Footer />
     </BrowserRouter>
   );
 }
