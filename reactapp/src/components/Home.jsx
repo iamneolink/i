81c5d26318db36fa1bf2177772276ce6a8d239a8
Pradeep Chandran M <pradeepchandranm@gmail.com>
2025-08-21 15:20:13 +0530

    img fixx

--- reactapp/src/components/Home.jsx
+++ reactapp/src/components/Home.jsx
@@ -4,19 +4,14 @@ import cricketImg from "../assets/background.jpg";
 function Home() {
   const navigate = useNavigate();
   return (
-    <div className="home-bg-wrapper">
-      <div
-        className="home-bg-blur"
-        style={{ backgroundImage: `url(${cricketImg})` }}
-      />
-      <div className="home-content">
-        <h2 className="home-welcome">
-          Welcome to Neo Cricket Tournament Registration
-        </h2>
-        <button className="register-btn" onClick={() => navigate("/add")}>
-          Register Player
-        </button>
-      </div>
+    <div className="home-content">
+      <h2 className="home-welcome">
+        Welcome to Cricket Tournament Registration
+      </h2>
+      <img src={cricketImg} alt="Cricket Tournament" />
+      <button className="register-btn" onClick={() => navigate("/add")}>
+        Register Player
+      </button>
     </div>
   );
 }
